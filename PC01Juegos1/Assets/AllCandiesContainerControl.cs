e4d3dba49c4bed0e1f9f1ec2c82cf25f189ae93f
Niltonmc <30881580+Niltonmc@users.noreply.github.com>
2020-04-18 16:13:59 -0500

    InitialCommit

---
+++ PC01Juegos1/Assets/AllCandiesContainerControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllCandiesContainerControl : MonoBehaviour {
+
+	public GameObject candyPref;
+	public List<Sprite> allPosibleSprites;
+	public Vector2 posToCreateCandy;
+	public int initialCandiesCount;
+	public List<CandyControl> allCandiesList;
+
+	public float timeToActivateCandy;
+	public List<CandyControl> allDeactivateCandiesList;
+	// Use this for initialization
+	void Start () {
+		GenerateAllCandies ();
+		Invoke ("ActivateCandies", timeToActivateCandy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void GenerateAllCandies(){
+		GameObject prefTmp;
+		CandyControl candyTmp;
+		int currentCandy = 0;
+		for (int i = 0; i < initialCandiesCount; ++i) {
+			prefTmp = Instantiate (candyPref, posToCreateCandy, transform.rotation);
+			prefTmp.transform.SetParent (this.transform);
+			candyTmp = prefTmp.GetComponent<CandyControl> ();
+			candyTmp.SetInitialValues (allPosibleSprites [currentCandy], currentCandy, GetComponent<AllCandiesContainerControl>());
+			currentCandy = currentCandy + 1;
+			if (currentCandy >= allPosibleSprites.Count) {
+				currentCandy = 0;
+			}
+			//allCandiesList.Add (candyTmp);
+			allDeactivateCandiesList.Add (candyTmp);
+			prefTmp.gameObject.SetActive (false);
+		}
+
+		//allDeactivateCandiesList = allCandiesList;
+	}
+
+	void ActivateCandies(){
+		CandyControl tmp;
+		int index = Random.Range (0, allDeactivateCandiesList.Count);
+		tmp = allDeactivateCandiesList [index];
+		tmp.gameObject.SetActive (true);
+		tmp.StartMovement ();
+		allDeactivateCandiesList.RemoveAt (index);
+		allCandiesList.Add (tmp);
+		Invoke ("ActivateCandies", timeToActivateCandy);
+	}
+
+	public void ReturnToDeactivate(CandyControl candy){
+		CandyControl tmp = candy;
+		candy.gameObject.SetActive (false);
+		allCandiesList.Remove (candy);
+		allDeactivateCandiesList.Add (tmp);
+	}
+}
---
+++ PC01Juegos1/Assets/PlayerControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float playerSpeedMovement;
+	private float vertical;
+	private Rigidbody2D rbPlayer;
+
+	public float minPosY;
+	public float maxPosY;
+	private float currentPosY;
+
+	public int playerLives;
+	// Use this for initialization
+	void Start () {
+		GetInitialComponent ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentPosY = Mathf.Clamp(transform.position.y, minPosY, maxPosY);
+		rbPlayer.position = new Vector2 (rbPlayer.position.x, currentPosY);
+	}
+
+	void FixedUpdate () {
+		vertical = Input.GetAxisRaw ("Vertical");
+		rbPlayer.velocity = new Vector2 (rbPlayer.velocity.x, playerSpeedMovement * vertical);
+	}
+
+	void GetInitialComponent () {
+		rbPlayer = GetComponent<Rigidbody2D> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Candy") {
+			CandyControl candy;
+			candy = other.GetComponent<CandyControl> ();
+			switch (candy.candyType) {
+			case 1:
+				playerLives = playerLives - 3;
+				break;
+			case 2:
+				playerLives = playerLives + 2;
+				break;
+			case 3:
+				playerLives = 0;
+				break;
+			case 4:
+				playerLives = playerLives - 1;
+				break;
+			default:
+				playerLives = playerLives - 1;
+				break;
+			}
+			candy.ReturnToDeactivate ();
+			print ("La cantidad de vidas es de: " + playerLives);
+			if (playerLives <= 0) {
+				SceneManager.LoadScene ("GameOver");
+			}
+		}
+	}
+}
